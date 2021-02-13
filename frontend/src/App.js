3c571b6cfdaa0902c3b5d800bff7704929375de2
AlexanderMukas <alexandermukas@yahoo.com>
2021-02-14 01:01:04 +0200

    create init header and footer components commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <>
+    <main>
+
+      <Header />
+
       <h1>Welcome to my Shop! Users</h1>
+      
+      <Footer />
+
+    </main>
+      
     </>
   );
 }
