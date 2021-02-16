8aad8c2d018dad1148bac90081e7690bf49cca71
AlexanderMukas <alexandermukas@yahoo.com>
2021-02-16 18:45:13 +0200

    first Card version end commit

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,6 +1,7 @@
 // rafce + tab
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import products from '../products';
 
 const Product = ( {product} ) => {
     return (
@@ -8,6 +9,25 @@ const Product = ( {product} ) => {
             <a href={`/product/${product._id}`}>
                 <Card.Img src={product.image} variant='top' />
             </a>
+            <Card.Body>
+
+                <a href={`/product/${product._id}`}>
+                    <Card.Title as='div'>
+                        <strong>{product.name}</strong>
+                    </Card.Title>
+                </a>
+
+                <Card.Text as='div'>
+                        {/* <Rating /> */}
+                        <div className='my-3'>
+                            {product.rating} from {product.numReviews} reviews
+                        </div>
+                </Card.Text>
+
+                <Card.Text as='h3'>
+                    ${product.price}
+                </Card.Text>
+            </Card.Body>
 
         </Card>
         
