6ce874fb3779329461e72d62232dc6f9e2086f5c
AlexanderMukas <alexandermukas@yahoo.com>
2021-02-16 15:22:56 +0200

    edit footer commit

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,17 @@
 // rafce + tab
 import React from 'react'
+import { Container, Row, Col} from 'react-bootstrap';
 
 const Footer = () => {
     return (
         <footer>
-            footer
+            <Container>
+                <Row>
+                    <Col className='text-center py-3'>
+                        Copyright &copy; Shop
+                    </Col>
+                </Row>
+            </Container>
         </footer> 
     )
 }
