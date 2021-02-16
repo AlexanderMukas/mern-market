15efeb49e528415d35764e32e4debb3871643030
AlexanderMukas <alexandermukas@yahoo.com>
2021-02-16 15:43:14 +0200

    edit header commit

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,21 @@
 // rafce + tab
 import React from 'react'
+// import { Navbar, NavDropdown, Nav, Form, FormControl, Button} from 'react-bootstrap';
+import { Navbar, Nav, Container} from 'react-bootstrap';
 
 const Header = () => {
     return (
         <header>
-            header
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Navbar.Brand href="/">Shop</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/cart">Cart</Nav.Link>
+                        <Nav.Link href="/login">Sign In</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+                </Navbar>
         </header>
     )
 }
