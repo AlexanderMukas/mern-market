813599a622adbdbafa45e27b1177a7cbbd5dbeab
AlexanderMukas <alexandermukas@yahoo.com>
2021-02-17 18:51:15 +0200

    add ProductScreen component and Links commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -18,6 +19,8 @@ const App = () => {
 
           {/* <HomeScreen /> */}
           <Route path='/' component={HomeScreen} exact />
+
+          <Route path='/product/:id' component={ProductScreen} />
 
         </Container>
       </main> 
