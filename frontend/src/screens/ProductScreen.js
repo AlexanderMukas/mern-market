d8ef9bb3ef70d5c17a1ded5c76103a1b770f57c6
AlexanderMukas <alexandermukas@yahoo.com>
2021-02-18 23:51:06 +0200

    Out of Stock check commit

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -72,7 +72,7 @@ const ProductScreen = ( {match} ) => {
                             </ListGroup.Item>
 
                             <ListGroup.Item>
-                                <Button className='btn-block' type='button'>
+                                <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
                                     Add to Cart
                                 </Button>
                             </ListGroup.Item>
