ef0bcdbdc52981fcbb4f992e3e1130c7de4293ea
AlexanderMukas <alexandermukas@yahoo.com>
2021-02-19 00:36:36 +0200

    test data fetch commit

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require('express'); // commonJS
+const products = require('./data/products');
 const app = express();
 
 const PORT = 5000;
 
 app.get('/', (req, res) => {
     res.send('API is running...')
-})
+});
+
+app.get('/api/products', (req, res) => {
+    res.json(products)
+});
 
 app.listen( PORT, console.log(`Server running on port ${PORT}...`) );
