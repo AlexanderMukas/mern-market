e06bee6c7bf7d38ecede7f9b807c814dc8dd0a56
AlexanderMukas <alexandermukas@yahoo.com>
2021-02-18 23:15:52 +0200

    add imports and find product commit

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from '../components/Rating';
+import products from '../products';
 
-const ProductScreen = () => {
+const ProductScreen = ( {match} ) => {
+
+    // find single product, params = :id in URL, backend will do it later    
+    const product = products.find( product => product._id === match.params.id);
+    // console.log(product.brand);
+    
+    
     return (
         <div>
-            Product
+            {product.name}
         </div>
     )
 }
