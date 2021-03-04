27c69fd5175c0871fdaa81701ddc178ce4e06288
AlexanderMukas <alexandermukas@yahoo.com>
2021-03-05 01:04:09 +0200

    product and order Schema commit

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,5 +1,15 @@
 import mongoose from 'mongoose';
 
+const reviewSchema = mongoose.Schema({
+    name: { type: String, required: true},
+    rating: { type: Number, required: true},
+    description: { type: String, required: true}
+}, 
+    {
+        timestamps: true
+    }
+);
+
 const productSchema = mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
@@ -20,9 +30,36 @@ const productSchema = mongoose.Schema({
     },
     category: {
         type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    reviews: [reviewSchema],
+    rating: {
+        type: Number,
         required: true,
-    }
+        default: 0
+    },
+    numReviews: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+        default: 0
+    },
 
+}, {
+    timestamps: true
 });
 
 const Product = mongoose.model('Product', productSchema);
