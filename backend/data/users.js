e30859fedbd65aa064272fe6f5cadb0f35e23918
AlexanderMukas <alexandermukas@yahoo.com>
2021-03-05 23:36:16 +0200

    add user data file commit

---
+++ backend/data/users.js
@@ -0,0 +1,19 @@
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: xxxx,
+        isAdmin: true
+    },
+    {
+        name: 'Alex Ivanov',
+        email: 'a.ivanov@example.com',
+        password: xxxx
+    },
+    {
+        name: 'Natalia Shevchenko',
+        email: 'n.shevchenko@example.com',
+        password: xxxx
+    },
+
+]
