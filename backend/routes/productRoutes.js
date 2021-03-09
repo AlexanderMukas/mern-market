028511aea092ce2eb818fd81b6848926473379fb
AlexanderMukas <alexandermukas@yahoo.com>
2021-03-09 22:54:33 +0200

    complete prod routes commit

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,29 +5,33 @@ const router = express.Router();
 
 import Product from '../models/productModel.js';
 
-// path '/api/products'...
+// @desc        Fetch all products
+// @route       GET /api/products
+// @access      Public
 
-router.get('/', asyncHandler(async (req, res) => {
-    // find all products
-    const products = await Product.find( {} );
+router.get(
+    '/', 
+    asyncHandler(async (req, res) => {
+        // find all products
+        const products = await Product.find( {} );
 
-    res.json(products)
+        res.json(products)
 }));
 
 
-router.get('/:id', (req, res) => {
-    const product = products.find( prod => prod._id === req.params.id);
-    res.json(product);
-});
+router.get(
+    '/:id', 
+    asyncHandler(async (req, res) => {
+    
+        const product = await Product.findById(req.params.id);
 
-
-
-
-
-
-
-
-
+        if(product){
+            res.json(product);
+        } else {
+            res.status(404).json( { message: 'Product not found'} );
+        }
+    
+}));
 
 
 export default router;
