a02aed0a0a086b42b99b830977db66ca0de7372c
AlexanderMukas <alexandermukas@yahoo.com>
2021-03-10 23:52:55 +0200

    create error middleware commit

--- backend/server.js
+++ backend/server.js
@@ -50,21 +50,10 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 
 
-app.use( (req, res, next) => {
-    const error = new Error(`Not found - ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-});
+app.use( );
 
 
-app.use( (err, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    res.status(statusCode);
-    res.json( {
-        message : err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-    })
-})
+app.use()
 
 // ------------------products with routes -------------------------------
 
