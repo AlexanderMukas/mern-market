04d9f84436de504057e8f5890fcea48d0c5a32c6
AlexanderMukas <alexandermukas@yahoo.com>
2021-03-13 19:03:29 +0200

    add one reducer in store commit

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const reducer = combineReducers( {} );
+//add reducer
+import { productListReducer } from './reducers/productReduces.js'
+
+const reducer = combineReducers({
+    productList: productListReducer,
+});
 
 const initialState = {};
 
