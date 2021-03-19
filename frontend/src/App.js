c9bf13e976a747f7a9657ba63a7297b32503d476
AlexanderMukas <alexandermukas@yahoo.com>
2021-03-19 20:51:31 +0200

    some fix in route commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@ const App = () => {
 
           <Route path='/product/:id' component={ProductScreen} />
 
-          <Route path='/cart/' component={CartScreen} />
+          <Route path='/cart/id:?' component={CartScreen} />
 
         </Container>
       </main> 
