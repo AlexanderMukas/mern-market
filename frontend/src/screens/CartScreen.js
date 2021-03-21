cf76d8307aaadce05fa952cd1b54e3cc980e4b59
AlexanderMukas <alexandermukas@yahoo.com>
2021-03-22 00:24:10 +0200

    edit CartScreen commit

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -6,7 +6,26 @@ import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 
 import Message from '../components/Message';
 
-const CartScreen = () => {
+import { addToCart } from '../actions/cartActions';
+
+// history - for redirect
+const CartScreen = ( { match, location, history } ) => {
+    const productId = match.params.id;
+    // const qty = match.params.qty;
+    // location.search => ?qty=1
+
+    const qty = location.search ? Number( location.search.split('=')[1] ) : 1
+    // console.log(qty); 
+    const dispatch = useDispatch();
+
+    useEffect( async () => {
+        if(productId) {
+            dispatch( addToCart(productId, qty) );
+        } 
+        
+    }, [dispatch, productId, qty]);
+
+
     return (
         <div>
         <h1>Cart</h1>
