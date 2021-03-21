ad58039be630aa936a4507b22913adaf6fa7257b
AlexanderMukas <alexandermukas@yahoo.com>
2021-03-21 23:47:29 +0200

    create add to cart action commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,7 +27,7 @@ const App = () => {
 
           <Route path='/product/:id' component={ProductScreen} />
 
-          <Route path='/cart/id:?' component={CartScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
 
         </Container>
       </main> 
