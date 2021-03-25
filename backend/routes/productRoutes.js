97ba053997ad8df7e6c4a2205dd893ef4c06016e
AlexanderMukas <alexandermukas@yahoo.com>
2021-03-25 17:14:33 +0200

    delete comments in routes commit

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,17 +7,6 @@ import {
     getProductById  
 } from '../controllers/productController.js'
 
-// @desc        Fetch all products
-// @route       GET /api/products/
-// @access      Public
-// router.get('/', getProducts);
-
-
-// @desc        Fetch single product by ID
-// @route       GET /api/products/:id
-// @access      Public
-// router.get('/:id', getProductById);
-
 router.route('/').get(getProducts);
 router.route('/:id').get(getProductById);
 
