76dc442948d5cd68b926d3a73522b82f4ba84f16
AlexanderMukas <alexandermukas@yahoo.com>
2021-03-25 17:11:42 +0200

    add controllers for routes commit

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,43 +1,24 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
-
 const router = express.Router();
 
-import Product from '../models/productModel.js';
+//add controllers
+import { 
+    getProducts, 
+    getProductById  
+} from '../controllers/productController.js'
 
 // @desc        Fetch all products
 // @route       GET /api/products/
 // @access      Public
-router.get(
-    '/', 
-    asyncHandler(async (req, res) => {
-        // find all products
-        const products = await Product.find( {} );
-
-        // res.status(401)
-        // throw new Error('Not Authorized')
-
-        res.json(products)
-}));
+// router.get('/', getProducts);
 
 
 // @desc        Fetch single product by ID
 // @route       GET /api/products/:id
 // @access      Public
-router.get(
-    '/:id', 
-    asyncHandler(async (req, res) => {
-    
-        const product = await Product.findById(req.params.id);
+// router.get('/:id', getProductById);
 
-        if(product){
-            res.json(product);
-        } else {
-            res.status(404);
-            throw new Error('Product not found!.');
-        }
-    
-}));
-
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
