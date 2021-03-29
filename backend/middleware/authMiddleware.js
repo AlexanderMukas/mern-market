d8ead63834042139fa0c6b89dcb64147d71bfec3
AlexanderMukas <alexandermukas@yahoo.com>
2021-03-29 16:53:49 +0300

    complete auth middleware commit

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,13 +1,41 @@
 //validate the token
 import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js'
 
-const protect = async (req, res, next) => {
+const protect = asyncHandler( async (req, res, next) => {
     let token;
+    
+    if( 
+        req.headers.authorization && 
+        req.headers.authorization.startsWith('Bearer') 
+    ) {
+        try {
+            token = (req.headers.authorization).split(' ')[1];
 
-    console.log(req.header.authorization);
 
-    next();
-}
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            // console.log(decoded); // { id: '6047da4cd853c880e2c69e22', iat: 1617023926, exp: 1619615926 }
+
+            req.user = await User.findById( decoded.id ).select('-password');
+
+            next();
+
+        } catch (error) {
+            console.error(error);
+            res.status(401);
+            throw new Error('Not authorized, token failed');
+            
+        }
+    }
+
+    if(!token) {
+        res.status(401);
+        throw new Error('Not authorized, no token');
+    }
+
+    // next();
+})
 
 export { protect };
