712f45a055baf2d528369dd430227ff520b89f7a
AlexanderMukas <alexandermukas@yahoo.com>
2021-03-29 17:35:30 +0300

    add resiter routes commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -31,14 +31,62 @@ const authUser = asyncHandler( async (req, res) => {
 });
 
 
+// @desc        Register new user
+// @route       POST /api/users
+// @access      Public
+const registerUser = asyncHandler( async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne( { email } );  // { email : email}
+
+    if(userExists){
+        res.status(400);
+        throw new Error('User already exists')    
+    }
+
+    const user = await User.create( {name, email, password} );
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        })
+
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+    
+
+});
+
+
 // @desc        Get user profile
 // @route       GET /api/users/profile
 // @access      Private
 const getUserProfile = asyncHandler( async (req, res) => {
-    // const user = await User.findById(req.user._id); 
-    res.send('Success!');
+
+    // req.user._id -->> from the authMiddleware
+    const user = await User.findById(req.user._id); 
+
+    if(user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        });
+
+    } else {
+        res.status(404);
+        throw new Error('User not found...')
+    }
+
+    
 });
 
 
 
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
