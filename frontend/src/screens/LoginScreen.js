c8592908ae22741b5002a3a4e2f9186b6311d533
AlexanderMukas <alexandermukas@yahoo.com>
2021-04-03 23:42:16 +0300

    create Login Screen commit

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+
+const LoginScreen = () => {
+    return (
+        <>
+            <Form>
+
+            
+                <Row>
+                    <Col>
+                    
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                    
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                    
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                    
+                    </Col>
+                </Row>
+            </Form>
+            
+        </>
+    )
+}
+
+export default LoginScreen
