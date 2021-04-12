15885ea8b42e9a09a982bcfd31037e9102f0595e
AlexanderMukas <alexandermukas@yahoo.com>
2021-04-12 20:31:34 +0300

    some fix user details profile commit

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -57,10 +57,10 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
     switch(action.type) {
 
         case USER_DETAILS_REQUEST:
-            return { loading: true }
+            return { ...state, loading: true }
 
         case USER_DETAILS_SUCCESS:
-            return { loading: false, userInfo: action.payload }
+            return { loading: false, user: action.payload }
 
         case USER_DETAILS_FAIL:
             return { loading: false, error: action.payload }
