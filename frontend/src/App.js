a36aaef8edf6e66a561d7619c51676fd68353f6f
AlexanderMukas <alexandermukas@yahoo.com>
2021-04-14 21:45:22 +0300

    add route to profile commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 
+import ProfileScreen from './screens/ProfileScreen';
+
 const App = () => {
   return (
     <Router>
@@ -25,9 +27,12 @@ const App = () => {
 
       <main className="py-3">
         <Container>
-      
+
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
+
+          <Route path='/profile' component={ProfileScreen} />
+
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/' component={HomeScreen} exact />
