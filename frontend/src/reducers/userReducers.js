bcc2aa782da37f4e667b0b3f3fe4d123b7d855ab
AlexanderMukas <alexandermukas@yahoo.com>
2021-04-17 22:57:27 +0300

    add user update reducer commit

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -8,7 +8,10 @@ import {
     USER_REGISTER_FAIL,
     USER_DETAILS_REQUEST,
     USER_DETAILS_SUCCESS,
-    USER_DETAILS_FAIL
+    USER_DETAILS_FAIL,
+    USER_DETAILS_UPDATE_FAIL,
+    USER_DETAILS_UPDATE_SUCCESS,
+    USER_DETAILS_UPDATE_REQUEST,
 } from '../constants/userConstants';
 
 // const initState = { 
@@ -63,6 +66,23 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
             return { loading: false, user: action.payload }
 
         case USER_DETAILS_FAIL:
+            return { loading: false, error: action.payload }
+
+        default:
+            return state;
+    }
+}
+
+export const userDetailsUpdateReducer = (state = { user: {} }, action) => {
+    switch(action.type) {
+
+        case USER_DETAILS_UPDATE_REQUEST:
+            return { ...state, loading: true }
+
+        case USER_DETAILS_UPDATE_SUCCESS:
+            return { loading: false, user: action.payload }
+
+        case USER_DETAILS_UPDATE_FAIL:
             return { loading: false, error: action.payload }
 
         default:
