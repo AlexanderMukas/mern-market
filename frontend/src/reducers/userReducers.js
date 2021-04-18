b5fd879a17f775938cafd69aa5af13a07698141f
AlexanderMukas <alexandermukas@yahoo.com>
2021-04-18 18:14:32 +0300

    fix names of constants and reducer commit

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -9,10 +9,10 @@ import {
     USER_DETAILS_REQUEST,
     USER_DETAILS_SUCCESS,
     USER_DETAILS_FAIL,
-    USER_UPDATE_REQUEST,
-    USER_UPDATE_SUCCESS,
-    USER_UPDATE_FAIL,
-    USER_UPDATE_RESET,
+    USER_UPDATE_PROFILE_REQUEST,
+    USER_UPDATE_PROFILE_SUCCESS,
+    USER_UPDATE_PROFILE_FAIL,
+    USER_UPDATE_PROFILE_RESET,
 } from '../constants/userConstants';
 
 // const initState = { 
@@ -74,16 +74,16 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
     }
 }
 
-export const userDetailsUpdateReducer = (state = { user: {} }, action) => {
+export const userUpdateProfileReducer = (state = { }, action) => {
     switch(action.type) {
 
-        case USER_UPDATE_REQUEST:
-            return { ...state, loading: true }
+        case USER_UPDATE_PROFILE_REQUEST:
+            return { loading: true }
 
-        case USER_UPDATE_SUCCESS:
-            return { loading: false, user: action.payload }
+        case USER_UPDATE_PROFILE_SUCCESS:
+            return { loading: false, success: true, userInfo: action.payload }
 
-        case USER_UPDATE_FAIL:
+        case USER_UPDATE_PROFILE_FAIL:
             return { loading: false, error: action.payload }
 
         default:
