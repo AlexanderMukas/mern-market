85b1d6ce27e49e6453f9a29a19ff3ef013bc3664
AlexanderMukas <alexandermukas@yahoo.com>
2021-04-18 18:21:13 +0300

    change update profile reducer commit

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -130,3 +130,43 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
         })
     }
 }
+
+// getState for send the TOKEN
+export const updateUserProfile = (user) => async (dispatch, getState) => {
+    try {
+        
+        dispatch({ type: USER_UPDATE_PROFILE_REQUEST })
+
+        // this information from Redux from ALL STATE
+        const { userLogin: { userInfo } } = getState();
+
+        const config = {
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+
+        //pass user in PUT request in Mongo
+        const { data } = await axios.put(
+            `/api/users/profile`,
+            user, 
+            config 
+        );
+
+        dispatch({
+            type: USER_DETAILS_SUCCESS,
+            payload: data
+        })
+
+    } catch (error) {
+        dispatch({
+            type: USER_DETAILS_FAIL,
+            payload: 
+                error.response && error.response.data.message 
+                    ? error.response.data.message 
+                    : error.message
+        })
+    }
+}
