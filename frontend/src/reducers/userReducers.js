679b41e238d9d40ff3b4d3f6b964da91384f833e
AlexanderMukas <alexandermukas@yahoo.com>
2021-04-18 18:10:58 +0300

    fix constants and importing in reducer commit

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -9,9 +9,10 @@ import {
     USER_DETAILS_REQUEST,
     USER_DETAILS_SUCCESS,
     USER_DETAILS_FAIL,
-    USER_DETAILS_UPDATE_FAIL,
-    USER_DETAILS_UPDATE_SUCCESS,
-    USER_DETAILS_UPDATE_REQUEST,
+    USER_UPDATE_REQUEST,
+    USER_UPDATE_SUCCESS,
+    USER_UPDATE_FAIL,
+    USER_UPDATE_RESET,
 } from '../constants/userConstants';
 
 // const initState = { 
@@ -76,13 +77,13 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
 export const userDetailsUpdateReducer = (state = { user: {} }, action) => {
     switch(action.type) {
 
-        case USER_DETAILS_UPDATE_REQUEST:
+        case USER_UPDATE_REQUEST:
             return { ...state, loading: true }
 
-        case USER_DETAILS_UPDATE_SUCCESS:
+        case USER_UPDATE_SUCCESS:
             return { loading: false, user: action.payload }
 
-        case USER_DETAILS_UPDATE_FAIL:
+        case USER_UPDATE_FAIL:
             return { loading: false, error: action.payload }
 
         default:
