6c7c15eb91a5b53afddc1c00adf6d3524002d1d3
AlexanderMukas <alexandermukas@yahoo.com>
2021-05-01 23:56:41 +0300

    create action save shipping address commit

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
+import { CART_ADD_ITEM,
+         CART_REMOVE_ITEM,
+         CART_SAVE_SHIPPING_ADDRESS
+} from '../constants/cartConstants';
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
     const { data } = await axios.get(`/api/products/${id}`);
@@ -25,6 +28,17 @@ export const removeFromCart = (id) => async (dispatch, getState) => {
     dispatch({
         type: CART_REMOVE_ITEM,
         payload: id
+    })
+
+    localStorage.setItem('cartItems', JSON.stringify( getState().cart.cartItems ));
+}
+
+export const saveShippingAddress = (data) => async (dispatch) => {
+    // const { data } = await axios.get(`/api/products/${id}`);
+
+    dispatch({
+        type: CART_SAVE_SHIPPING_ADDRESS,
+        payload: data
     })
 
     localStorage.setItem('cartItems', JSON.stringify( getState().cart.cartItems ));
