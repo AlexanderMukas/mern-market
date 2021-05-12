f34d0dc913a99d5dbd2c99dc4dfb93294fcde88b
AlexanderMukas <alexandermukas@yahoo.com>
2021-05-12 23:59:53 +0300

    step1 links commit

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Nav } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import { Link } from 'react-router-dom'
 
 const CheckoutSteps = ( { step1, step2, step3, step4 } ) => {
     return (
         <Nav className='justify-content-center mb-4'>
+            <Nav.Item>
+                { step1 ? (
+                    <LinkContainer to='/login'>
+                        <Nav.Link>Sign In</Nav.Link>
+                    </LinkContainer>
+                ) : <Nav.Link disabled></Nav.Link> }
+            </Nav.Item>
             
         </Nav>
     )
