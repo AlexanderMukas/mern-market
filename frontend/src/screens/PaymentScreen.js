653f006cb2df237437c73ad36fb160ce86668970
AlexanderMukas <alexandermukas@yahoo.com>
2021-05-20 23:43:43 +0300

    fixing payment screen commit

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -40,44 +40,7 @@ const PaymentScreen = ( { history } ) => {
 
            <h1>Payment Method</h1>
            <Form onSubmit={submitHandler}>
-                {/* ---ADDRESS--- */}
-                <Form.Group controlId='address'>
-                    <Form.Label>Address</Form.Label>
-                    <Form.Control 
-                        type='text' 
-                        placeholder='Enter city address'
-                        value={address}
-                        required
-                        onChange={ (e) => setAddress(e.target.value) }
-                    >
-                    </Form.Control>
-                </Form.Group>
-
-
-                {/* ---POSTALCODE--- */}
-                <Form.Group controlId='postalCode'>
-                    <Form.Label>Postal Code</Form.Label>
-                    <Form.Control 
-                        type='number' 
-                        placeholder='Enter postal code'
-                        value={postalCode}
-                        onChange={ (e) => setPostalCode(e.target.value)}
-                    > 
-                    </Form.Control>
-                </Form.Group>
-
-                {/* ---COUNTRY--- */}
-                <Form.Group controlId='country'>
-                    <Form.Label>Set country</Form.Label>
-                    <Form.Control 
-                        type='text' 
-                        placeholder='Country'
-                        value={country}
-                        onChange={ (e) => setCountry(e.target.value)}
-                    > 
-                    </Form.Control>
-                </Form.Group>
-
+            
                 <Button type='submit' variant='primary'>
                     Continue
                 </Button>
