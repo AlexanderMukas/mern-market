2c3a70e4563402f5d31c39f0cb2194f32b3d6f0e
AlexanderMukas <alexandermukas@yahoo.com>
2021-05-29 22:21:24 +0300

    add imports on Place Order Screen commit

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
+
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import CheckoutSteps from '../components/CheckoutSteps';
 
 const PlaceOrderScreen = () => {
     return (
