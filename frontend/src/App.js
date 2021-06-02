68f8a623301bde00aba7a1ac86ee87c38305e433
AlexanderMukas <alexandermukas@yahoo.com>
2021-06-02 23:43:59 +0300

    add some comments commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,11 +14,12 @@ import CartScreen from './screens/CartScreen';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-// 
+// Login/Registration
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 
 import ProfileScreen from './screens/ProfileScreen';
+// step1 step2 step3 step4
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
@@ -29,24 +30,24 @@ const App = () => {
     <Router>
         <Header />
 
-      <main className="py-3">
-        <Container>
+        <main className="py-3">
+          <Container>
 
-          <Route path='/shipping' component={ShippingScreen} />
-          <Route path='/placeorder' component={PlaceOrderScreen} />
-          <Route path='/payment' component={PaymentScreen} />
+            <Route path='/shipping' component={ShippingScreen} />
+            <Route path='/placeorder' component={PlaceOrderScreen} />
+            <Route path='/payment' component={PaymentScreen} />
 
-          <Route path='/login' component={LoginScreen} />
-          <Route path='/register' component={RegisterScreen} />
+            <Route path='/login' component={LoginScreen} />
+            <Route path='/register' component={RegisterScreen} />
 
-          <Route path='/profile' component={ProfileScreen} />
+            <Route path='/profile' component={ProfileScreen} />
 
-          <Route path='/product/:id' component={ProductScreen} />
-          <Route path='/cart/:id?' component={CartScreen} />
-          <Route path='/' component={HomeScreen} exact />
+            <Route path='/product/:id' component={ProductScreen} />
+            <Route path='/cart/:id?' component={CartScreen} />
+            <Route path='/' component={HomeScreen} exact />
 
-        </Container>
-      </main> 
+          </Container>
+        </main> 
 
         <Footer />
     </Router>
