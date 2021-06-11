8c6f8f4e083b54568d31dab15ca22fb8f3dcbbca
AlexanderMukas <alexandermukas@yahoo.com>
2021-06-11 23:56:48 +0300

    edit order routes commit

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,2 +1,18 @@
 import express from 'express';
 const router = express.Router();
+
+//add controller
+import addOrderItems from '../controllers/orderController';
+
+import { protect } from '../middleware/authMiddleware.js';
+
+router.route('/').post(registerUser);
+router.post('/login', authUser);
+
+// put middleware on first argument first
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+
+// router.route('/profile').put(protect, updateUserProfile);
+
+export default router;
+
