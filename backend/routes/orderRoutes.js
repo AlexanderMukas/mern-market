af3b2a4732b77a891a1662d2739ee3964ce957db
AlexanderMukas <alexandermukas@yahoo.com>
2021-07-15 23:58:54 +0300

    add route order by id commit

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 const router = express.Router();
 
-import { addOrderItems } from '../controllers/orderController.js';
+import { addOrderItems, getOrderById } from '../controllers/orderController.js';
 
 import { protect } from '../middleware/authMiddleware.js';
 
 // add protect middleware in route
 router.route('/').post(protect, addOrderItems);
+router.route('/:id').get(protect, getOrderById);
 
 export default router;
 
