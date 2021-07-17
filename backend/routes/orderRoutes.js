ce4c0a0ade72552d2ffcb171aac907a7daca34c9
AlexanderMukas <alexandermukas@yahoo.com>
2021-07-17 23:48:42 +0300

    add comment commit

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -6,6 +6,7 @@ import { addOrderItems, getOrderById } from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 // add protect middleware in route
+// url/orders/
 router.route('/').post(protect, addOrderItems);
 router.route('/:id').get(protect, getOrderById);
 
