6f2a71f63b94ce210582b877da256fd9aa24dcac
AlexanderMukas <alexandermukas@yahoo.com>
2021-07-21 00:12:47 +0300

    fix orderDetails reducer commit

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -33,15 +33,17 @@ export const orderCreateReducer = (state = {} , action ) => {
     }
 }
 
-export const orderDetailsReducer = (state = {id} , action ) => {
+export const orderDetailsReducer = (state = { orderItems: [] , shippingAddress: { } } , action ) => {
     switch(action.type) {
         case ORDER_DETAILS_REQUEST:
-            return { loading: true }
+            return { 
+                ...state, 
+                loading: true 
+            }
 
         case ORDER_DETAILS_SUCCESS:
             return { 
                 loading: false, 
-                success : true,
                 order: action.payload }
 
         case ORDER_DETAILS_FAIL:
