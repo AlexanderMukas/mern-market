f7d467649fc2c3ea126865bd585b5985f4df90fa
AlexanderMukas <alexandermukas@yahoo.com>
2021-07-23 23:54:09 +0300

    create the order screen commit

---
+++ frontend/src/screens/OrderScreen.js
@@ -0,0 +1,17 @@
+import React, { useEffect, } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
+
+import Message from '../components/Message';
+
+const OrderScreen = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default OrderScreen;
