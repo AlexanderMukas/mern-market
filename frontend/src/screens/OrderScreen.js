8ca7ed48b5234cdcd3ccd8c2c0c6d975b574fd02
AlexanderMukas <alexandermukas@yahoo.com>
2021-07-24 00:22:16 +0300

    editing order screen commit

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,19 +1,142 @@
-import React, { useEffect, } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
+
 import { useDispatch, useSelector } from 'react-redux';
 
-import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
-
 import Message from '../components/Message';
+import Loader from '../components/Message';
+
+import { Link } from 'react-router-dom';
 
 import { getOrderDetails } from '../actions/orderActions';
 
-const OrderScreen = ( { history, match } ) => {
-    return (
-        <div>
-            
-        </div>
-    )
+const OrderScreen = ( { match } ) => {
+    const orderId = match.params.id;
+
+    const dispatch = useDispatch();
+
+    const cart = useSelector( state => state.cart);
+    
+    const orderDetails = useSelector( state => state.orderDetails )
+    const { order, loading, error } = orderDetails; 
+
+    useEffect( () => {
+        dispatch(getOrderDetails(orderId))
+    }, [orderId])
+    
+    return loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : <> 
+        <h1>Order: {order._id}</h1>
+        <Row>
+                <Col md={8}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h2>Shipping</h2>
+                            <p>
+                                <strong>Address</strong>
+                                {cart.shippingAddress.address},
+                                {cart.shippingAddress.city},
+                                {cart.shippingAddress.postalCode},
+                                {cart.shippingAddress.country}
+                            </p>
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            <h2>Payment Method</h2>
+                            <strong>Method: </strong>
+                            {cart.paymentMethod}
+
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            <h2>Order Items</h2>
+                            {cart.cartItems.length === 0 
+                            ? <Message>Your Cart is empty</Message>
+                            : (
+                                <ListGroup variant='flush'>
+                                    {cart.cartItems.map( (item, index)=> (
+                                        <ListGroup.Item key={index}>
+                                            <Row>
+                                                {/* Image column */}
+                                                <Col md={1}>
+                                                    <Image src={item.image} alt={item.name} fluid rounded />
+                                                </Col>
+                                                
+                                                {/* Product name and link column */}
+                                                <Col>
+                                                    <Link to={`/product/${item.product}`}>
+                                                        {item.name}
+                                                    </Link>
+                                                </Col>
+
+                                                {/* Qty and price column */}
+                                                <Col md={4}>
+                                                    {item.qty} x ${item.price} = ${item.qty * item.price}
+                                                </Col>
+
+                                            </Row>
+                                            
+                                        </ListGroup.Item>
+                                    ))
+                                    }
+                                </ListGroup>
+                            )}
+
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={4}>
+                    <Card>
+                        <ListGroup variant='flush'>
+                            <ListGroup.Item>
+                                <h2>Order Summary</h2>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Items</Col>
+                                    <Col>${cart.itemsPrice}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Shipping</Col>
+                                    <Col>${cart.shippingPrice}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Tax</Col>
+                                    <Col>${cart.taxPrice}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Total</Col>
+                                    <Col>${cart.totalPrice}</Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            {/* error message */}
+                            <ListGroup.Item>
+                                {error && <Message variant='danger'>{error}</Message> }
+                            </ListGroup.Item>
+                            
+
+                            <ListGroup.Item>
+                                <Button
+                                  type='button'
+                                  className='btn-block'
+                                  disabled={cart.cartItems === 0}
+                                  onClick={placeOrderHandler}
+                                >
+                                    Place Order
+                                </Button>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+
+                </Col>
+            </Row>
+    </>
 }
 
-export default OrderScreen;
+export default OrderScreen
