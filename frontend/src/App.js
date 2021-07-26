27e610b33caf51ff7500908f7cf2df71951415b2
AlexanderMukas <alexandermukas@yahoo.com>
2021-07-26 23:56:31 +0300

    add to app order screen commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,6 +23,7 @@ import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 
 
 const App = () => {
@@ -35,6 +36,9 @@ const App = () => {
 
             <Route path='/shipping' component={ShippingScreen} />
             <Route path='/placeorder' component={PlaceOrderScreen} />
+            
+            <Route path='/orders/:id' component={OrderScreen} />
+
             <Route path='/payment' component={PaymentScreen} />
 
             <Route path='/login' component={LoginScreen} />
