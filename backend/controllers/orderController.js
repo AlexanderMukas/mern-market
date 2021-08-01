18cec605b664bda59f4f447e5d8bddb46a5ec82f
AlexanderMukas <alexandermukas@yahoo.com>
2021-08-01 17:00:13 +0300

    create controller - updateOrderToPaid commit

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -49,7 +49,7 @@ const addOrderItems = asyncHandler( async (req, res) => {
 const getOrderById = asyncHandler( async (req, res) => {
     
     const order = await Order.findById(req.params.id).populate('user', 'name email');
-    console.log(order);
+    // console.log(order);
 
     if(order){
         res.json(order);
@@ -60,7 +60,39 @@ const getOrderById = asyncHandler( async (req, res) => {
 })
 
 
+// @desc        Update order to paid
+// @route       GET /api/orders/:id/pay
+// @access      Private
+const updateOrderToPaid = asyncHandler( async (req, res) => {
+    
+    const order = await Order.findById(req.params.id);
+    // console.log(order);
+
+    if(order){
+        order.isPaid = true;
+        order.paidAt = Date.now();
+
+        //payment result from PayPal 
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address
+        }
+
+        //save in MongoDB this staf
+        const updatedOrder = await order.save();
+
+        res.json(updatedOrder);
+
+    } else {
+        res.status(404);
+        throw new Error('Order not found!');
+    }
+})
+
 export {
     addOrderItems,
-    getOrderById
+    getOrderById,
+    updateOrderToPaid
 }
