0481ea7d86583d305a48bced97a271b018f32dd9
AlexanderMukas <alexandermukas@yahoo.com>
2021-08-13 00:02:33 +0300

    fix controllers name commit

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -98,37 +98,16 @@ const updateOrderToPaid = asyncHandler( async (req, res) => {
 // @desc        Get logged in user orders
 // @route       GET /api/orders/myorders
 // @access      Private
-const showMyOrders = asyncHandler( async (req, res) => {
-
-    const orders = await Order.find();
-    
-    // console.log(order);
-
-    if(order){
-        order.isPaid = true;
-        order.paidAt = Date.now();
-
-        //payment result from PayPal 
-        order.paymentResult = {
-            id: req.body.id,
-            status: req.body.status,
-            update_time: req.body.update_time,
-            email_address: req.body.payer.email_address
-        }
-
-        //save in MongoDB this staf
-        const updatedOrder = await order.save();
-
-        res.json(updatedOrder);
-
-    } else {
-        res.status(404);
-        throw new Error('Order not found!');
-    }
+const getMyOrders = asyncHandler( async (req, res) => {
+    //find from MongoDB
+    const orders = await Order.find( { user: req.user._id } );
+    //send to frontend
+    res.json(orders)
 })
 
 export {
     addOrderItems,
     getOrderById,
-    updateOrderToPaid
+    updateOrderToPaid,
+    getMyOrders
 }
