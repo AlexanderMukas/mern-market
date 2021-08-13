e74aed504a13a0bfc430c1218f0c781e0abdcdd6
AlexanderMukas <alexandermukas@yahoo.com>
2021-08-14 00:40:18 +0300

    create listMyOrders action commit

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -9,7 +9,10 @@ import {
     ORDER_PAY_REQUEST,
     ORDER_PAY_SUCCESS,
     ORDER_PAY_FAIL,
-    ORDER_PAY_RESET
+    ORDER_PAY_RESET,
+    ORDER_LIST_MY_REQUEST,
+    ORDER_LIST_MY_SUCCESS,
+    ORDER_LIST_MY_FAIL
 } from '../constants/orderConstants';
 
 // создание заказа
@@ -119,6 +122,40 @@ export const payOrder = (orderId, paymentResult) => async (dispatch, getState) =
     } catch (error) {
         dispatch({
             type: ORDER_PAY_FAIL,
+            payload: 
+                error.response && error.response.data.message 
+                    ? error.response.data.message 
+                    : error.message
+        })
+    }
+}
+
+// оплата заказа
+export const listMyOrders = () => async (dispatch, getState) => {
+    try {
+
+        //for spinner "LOADING..."
+        dispatch({ type: ORDER_LIST_MY_REQUEST })
+
+        // this information from Redux store from ALL STATE
+        const { userLogin: { userInfo } } = getState();
+        // add token on config, GET request not need Content-type!!!
+        const config = {
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+        const { data } = await axios.get('/api/orders/myorders', config);
+
+        dispatch({
+            type: ORDER_LIST_MY_SUCCESS,
+            payload: data
+        })
+
+    } catch (error) {
+        dispatch({
+            type: ORDER_LIST_MY_FAIL,
             payload: 
                 error.response && error.response.data.message 
                     ? error.response.data.message 
