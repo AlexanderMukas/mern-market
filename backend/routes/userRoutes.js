6be9b9b24584c5055c6218fbc7750c45033b6457
AlexanderMukas <alexandermukas@yahoo.com>
2021-08-20 00:08:57 +0300

    create admin protect middleware commit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,12 +5,13 @@ import {
     authUser, 
     registerUser, 
     getUserProfile, 
-    updateUserProfile 
+    updateUserProfile,
+    getUsers
 } from '../controllers/userController.js';
 
-import { protect } from '../middleware/authMiddleware.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
-router.route('/').post(registerUser);
+router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/login', authUser);
 
 // put middleware on first argument first
