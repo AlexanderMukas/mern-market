83019136dfa265727eab6bac51bad1acad9845c3
AlexanderMukas <alexandermukas@yahoo.com>
2021-08-21 20:26:23 +0300

    add user list constants commit

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -13,7 +13,10 @@ import {
     USER_UPDATE_PROFILE_REQUEST,
     USER_UPDATE_PROFILE_SUCCESS,
     USER_UPDATE_PROFILE_FAIL,
-    USER_DETAILS_RESET
+    USER_DETAILS_RESET,
+    USER_LIST_REQUEST,
+    USER_LIST_SUCCESS,
+    USER_LIST_FAIL
 } from '../constants/userConstants';
 
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants';
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -13,7 +13,10 @@ import {
     USER_UPDATE_PROFILE_REQUEST,
     USER_UPDATE_PROFILE_SUCCESS,
     USER_UPDATE_PROFILE_FAIL,
-    USER_UPDATE_PROFILE_RESET
+    USER_UPDATE_PROFILE_RESET,
+    USER_LIST_REQUEST,
+    USER_LIST_SUCCESS,
+    USER_LIST_FAIL
 } from '../constants/userConstants';
 
 // const initState = { 
@@ -39,7 +42,6 @@ export const userLoginReducer = (state = { }, action) => {
             return state;
     }
 }
-
 
 export const userRegisterReducer = (state = { }, action) => {
     switch(action.type) {
