397dae920303173e5db12b031c91fa50a624bf14
AlexanderMukas <alexandermukas@yahoo.com>
2021-08-27 00:15:47 +0300

    create table buttons commit

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -44,9 +44,27 @@ const UserListScreen = () => {
                             <td>{user._id}</td>
                             <td>{user.name}</td>
                             <td><a href={`mailto:${user.email}`} >{user.email}</a></td>
-                            <td>{user.isAdmin}</td>
+                            <td>
+                                {user.isAdmin ? (<i className='fas fa-check' style={{color: 'green'}}></i>) 
+                                    : (<i className='fas fa-times' style={{ color: 'red'}}></i>)
+                                }
+                            </td>
                             
-                            <td>delete</td>
+                            {/* delete */}
+                            <td>
+                                <LinkContainer to={`/users/${user._id}/edit`}>
+                                    <Button variant='light' className='btn-sm'>
+                                        <i className='fas fa-edit'></i>
+                                    </Button>
+                                </LinkContainer>
+                                <Button 
+                                    variant='danger'
+                                    className='btn-sm'
+                                    onClick={() => deleteHandler(user._id)}
+                                >
+                                    <i className='fas fa-trash'></i>
+                                </Button>
+                            </td>
                         </tr>
                     ))}
                   </tbody>
