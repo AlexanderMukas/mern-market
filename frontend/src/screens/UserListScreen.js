9b2a389de462cecd558bf595c4df73dbe63cf40f
AlexanderMukas <alexandermukas@yahoo.com>
2021-08-29 20:23:00 +0300

    delete one hook commit

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useEffect} from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Table, Button, Tab } from 'react-bootstrap';
 
