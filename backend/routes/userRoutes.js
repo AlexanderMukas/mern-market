e3bcdb8982c13de08fcce7d6ce9455f87c747d67
AlexanderMukas <alexandermukas@yahoo.com>
2021-09-01 21:16:23 +0300

    some changes of code commit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,16 +11,16 @@ import {
 } from '../controllers/userController.js';
 
 import { protect, admin } from '../middleware/authMiddleware.js';
+// put middleware on first argument first
 
 //api/users/...
 router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/login', authUser);
-
-// put middleware on first argument first
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
-
-// router.route('/profile').put(protect, updateUserProfile);
-
+router
+    .route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile);
+    
 router.route('/:id').delete(protect, admin, deleteUser);
 
 export default router;
