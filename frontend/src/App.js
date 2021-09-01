36e8b17ae2e5834b538b4566094687ce0efbf577
AlexanderMukas <alexandermukas@yahoo.com>
2021-09-01 23:45:30 +0300

    add route to app udit user in table commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,6 +26,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 
 import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
 
 
 const App = () => {
@@ -50,7 +51,10 @@ const App = () => {
 
             <Route path='/product/:id' component={ProductScreen} />
             <Route path='/cart/:id?' component={CartScreen} />
+
             <Route path='/admin/userlist' component={UserListScreen} />
+            <Route path='/admin/user/:id/edit' component={UserEditScreen} />
+
             <Route path='/' component={HomeScreen} exact />
 
           </Container>
