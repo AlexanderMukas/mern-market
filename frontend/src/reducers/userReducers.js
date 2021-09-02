d39c0f38b9213c085a8828aa5c049aa30c7fc07a
AlexanderMukas <alexandermukas@yahoo.com>
2021-09-03 00:03:14 +0300

    fix name of const commit

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -151,9 +151,9 @@ export const userUpdateReducer = (state = { user: {} }, action) => {
         case USER_UPDATE_FAIL:
             return { loading: false, error: action.payload }
         
-        case USER_DETAILS_RESET:
+        case USER_UPDATE_RESET:
             return { user: {} }
-            
+
         default:
             return state;
     }
