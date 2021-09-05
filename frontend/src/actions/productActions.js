3625883322b20730987c19db2da2b7540db5837c
AlexanderMukas <alexandermukas@yahoo.com>
2021-09-06 00:00:00 +0300

    create upd and del product actions commit

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -5,7 +5,14 @@ import {
     PRODUCT_LIST_FAIL,
     PRODUCT_DETAILS_REQUEST, 
     PRODUCT_DETAILS_SUCCESS, 
-    PRODUCT_DETAILS_FAIL
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_UPDATE_REQUEST,
+    PRODUCT_UPDATE_SUCCESS,
+    PRODUCT_UPDATE_FAIL,
+    PRODUCT_UPDATE_RESET,
+    PRODUCT_DELETE_REQUEST,
+    PRODUCT_DELETE_SUCCESS,
+    PRODUCT_DELETE_FAIL
 } from '../constants/productConstants.js';
 
 
@@ -53,3 +60,69 @@ export const listProductDetails = (id) => async (dispatch) => {
         })
     }
 }
+
+export const updateProduct = (product) => async (dispatch, getState) => {
+    try {
+        dispatch({ type: PRODUCT_UPDATE_REQUEST })
+
+        // this information from Redux from ALL STATE
+        const { userLogin: { userInfo } } = getState();
+        const config = {
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        // only Admin can update users
+        const { data } = await axios.put(`/api/products/${product._id}`, product, config );
+
+        dispatch({ type: PRODUCT_UPDATE_SUCCESS });
+
+        //for updating user
+        dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_UPDATE_FAIL,
+            payload: 
+                error.response && error.response.data.message 
+                    ? error.response.data.message 
+                    : error.message
+        })
+    }
+}
+
+export const deleteProduct = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({ type: PRODUCT_DELETE_REQUEST })
+
+        // this information from Redux from ALL STATE
+        const { userLogin: { userInfo } } = getState();
+        const config = {
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        // only Admin can detele products
+        await axios.delete(`/api/products/${id}`, config );
+
+        dispatch({
+            type: PRODUCT_DELETE_SUCCESS
+            // payload: data
+        })
+
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_DELETE_FAIL,
+            payload: 
+                error.response && error.response.data.message 
+                    ? error.response.data.message 
+                    : error.message
+        })
+    }
+}
+
+
