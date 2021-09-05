d7caf3f2e31e4f9ce87dcfc0a066a1c564bc65c9
AlexanderMukas <alexandermukas@yahoo.com>
2021-09-05 23:36:03 +0300

    create upt, del product reducers commit

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -4,7 +4,14 @@ import {
     PRODUCT_LIST_FAIL,
     PRODUCT_DETAILS_REQUEST, 
     PRODUCT_DETAILS_SUCCESS, 
-    PRODUCT_DETAILS_FAIL
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_UPDATE_REQUEST,
+    PRODUCT_UPDATE_SUCCESS,
+    PRODUCT_UPDATE_FAIL,
+    PRODUCT_UPDATE_RESET,
+    PRODUCT_DELETE_REQUEST,
+    PRODUCT_DELETE_SUCCESS,
+    PRODUCT_DELETE_FAIL
 } from '../constants/productConstants';
 
 // const initialState = {
@@ -41,6 +48,43 @@ export const productDetailsReducer = (
         case PRODUCT_DETAILS_FAIL:
             return { loading: false, error: action.payload }
             
+        default:
+            return state;
+    }
+}
+
+export const productUpdateReducer = (state = { product: {} }, action) => {
+    switch(action.type) {
+
+        case PRODUCT_UPDATE_REQUEST:
+            return { loading: true }
+
+        case PRODUCT_UPDATE_SUCCESS:
+            return { loading: false, success: true }
+
+        case PRODUCT_UPDATE_FAIL:
+            return { loading: false, error: action.payload }
+        
+        case PRODUCT_UPDATE_RESET:
+            return { product: {} }
+
+        default:
+            return state;
+    }
+}
+
+export const productDeleteReducer = (state = { }, action) => {
+    switch(action.type) {
+
+        case PRODUCT_DELETE_REQUEST:
+            return { loading: true }
+
+        case PRODUCT_DELETE_SUCCESS:
+            return { loading: false, success: true }
+
+        case PRODUCT_DELETE_FAIL:
+            return { loading: false, error: action.payload }
+
         default:
             return state;
     }
