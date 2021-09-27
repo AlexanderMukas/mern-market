236878c96540cccf0e3d55b772af8ebb5083d978
AlexanderMukas <alexandermukas@yahoo.com>
2021-09-27 20:25:12 +0300

    create route get orders commit

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,14 +4,18 @@ const router = express.Router();
 import { addOrderItems, 
          getOrderById, 
          updateOrderToPaid,
-         getMyOrders
+         getMyOrders,
+         getOrders
 } from '../controllers/orderController.js';
 
-import { protect } from '../middleware/authMiddleware.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 // add protect middleware in route
 // url/orders/
-router.route('/').post(protect, addOrderItems);
+router.route('/')
+    .post(protect, addOrderItems)
+    .get(protect, admin, getOrders);
+    
 router.route('/myorders').get(protect, getMyOrders);
 router.route('/:id').get(protect, getOrderById);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
