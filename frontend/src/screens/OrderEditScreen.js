adf9c173260e3b6c7390a739fdfad6d38962e109
AlexanderMukas <alexandermukas@yahoo.com>
2021-10-02 23:59:53 +0300

    create order edit list screen commit

---
+++ frontend/src/screens/OrderEditScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OrderEditScreen = () => {
+    return (
+        <div>
+            Order Edit list screen!
+        </div>
+    )
+}
+
+export default OrderEditScreen
