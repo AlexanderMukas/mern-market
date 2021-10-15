bac5f2fdc2c1a489b3d89c1125d2d45b5c1f01c2
AlexanderMukas <alexandermukas@yahoo.com>
2021-10-16 00:40:25 +0300

    create deliver order action commit

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -10,6 +10,10 @@ import {
     ORDER_PAY_SUCCESS,
     ORDER_PAY_FAIL,
     ORDER_PAY_RESET,
+    ORDER_DELIVER_REQUEST,
+    ORDER_DELIVER_SUCCESS,
+    ORDER_DELIVER_FAIL,
+    ORDER_DELIVER_RESET,
     ORDER_LIST_MY_REQUEST,
     ORDER_LIST_MY_SUCCESS,
     ORDER_LIST_MY_FAIL,
@@ -129,6 +133,36 @@ export const payOrder = (orderId, paymentResult) => async (dispatch, getState) =
     } catch (error) {
         dispatch({
             type: ORDER_PAY_FAIL,
+            payload: 
+                error.response && error.response.data.message 
+                    ? error.response.data.message 
+                    : error.message
+        })
+    }
+}
+
+export const deliverOrder = (order) => async (dispatch, getState) => {
+    try {
+        dispatch({ type: ORDER_DELIVER_REQUEST })
+
+        const { userLogin: { userInfo } } = getState();
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+        
+        // Database PUT QUERY for update
+        const { data } = await axios.put(`/api/orders/${order._id}/deliver`, {}, config );
+
+        dispatch({
+            type: ORDER_DELIVER_SUCCESS,
+            payload: data
+        })
+
+    } catch (error) {
+        dispatch({
+            type: ORDER_DELIVER_FAIL,
             payload: 
                 error.response && error.response.data.message 
                     ? error.response.data.message 
