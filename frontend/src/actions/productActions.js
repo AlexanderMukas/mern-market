5cacc71f4c29bf5f936b9e7aa90d749027fe6db8
AlexanderMukas <alexandermukas@yahoo.com>
2021-10-24 00:17:30 +0300

    fix create prod review action commit

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -16,6 +16,10 @@ import {
     PRODUCT_CREATE_REQUEST,
     PRODUCT_CREATE_SUCCESS,
     PRODUCT_CREATE_FAIL,
+    PRODUCT_CREATE_REVIEW_REQUEST,
+    PRODUCT_CREATE_REVIEW_SUCCESS,
+    PRODUCT_CREATE_REVIEW_FAIL,
+    PRODUCT_CREATE_REVIEW_RESET,
     // PRODUCT_CREATE_RESET,
 } from '../constants/productConstants.js';
 
@@ -150,6 +154,36 @@ export const createProduct = () => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: PRODUCT_CREATE_FAIL,
+            payload: 
+                error.response && error.response.data.message 
+                    ? error.response.data.message 
+                    : error.message
+        })
+    }
+}
+
+export const createProductReview = (productId, review) => async (dispatch, getState) => {
+    try {
+        dispatch({ type: PRODUCT_CREATE_REVIEW_REQUEST })
+
+        // get state from store
+        const { userLogin: { userInfo } } = getState();
+        const config = {
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        await axios.post(`/api/products/${productId}/reviews`, review , config );
+
+        dispatch({ 
+            type: PRODUCT_CREATE_REVIEW_SUCCESS,
+        });
+
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_CREATE_REVIEW_FAIL,
             payload: 
                 error.response && error.response.data.message 
                     ? error.response.data.message 
