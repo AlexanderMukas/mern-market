1d5e342b975f2aed145e0a2a55583d47ad152b4d
AlexanderMukas <alexandermukas@yahoo.com>
2021-10-25 23:50:42 +0300

    render comment in prod screen commit

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -160,7 +160,19 @@ const ProductScreen = ( {history, match} ) => {
                     <Row>
                         <Col md={6}>
                             <h2>Reviews</h2>
-                            
+                            {product.reviews.length === 0 && <Message>No Reviews</Message>}
+                            <ListGroup variant='flush' >
+                                {product.reviews.map( review => (
+                                    <ListGroup.Item key={review._id}>
+                                        <strong>{review.name}</strong>
+
+                                        {/* ***** 5 stars */}
+                                        <Rating value={review.rating} />
+                                        <p>{review.createdAt.substring(0, 10)}</p>
+                                        <p>{review.comment}</p>
+                                    </ListGroup.Item>
+                                ))}
+                            </ListGroup>
                         </Col>
                     </Row>
                   </>
