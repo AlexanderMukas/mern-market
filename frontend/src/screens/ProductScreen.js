1b9a25dfcf8bb67f96ea40fcd2cc7abbb1c3a733
AlexanderMukas <alexandermukas@yahoo.com>
2021-10-28 00:20:02 +0300

    fix finally cust review commit

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -42,8 +42,15 @@ const ProductScreen = ( {history, match} ) => {
     const { userInfo } = userLogin;
 
     useEffect( async () => {
+        if(successProductReview) {
+            alert('Review Submitted!');
+            setRating(0);
+            setComment('');
+            dispatch({ type: PRODUCT_CREATE_REVIEW_RESET})
+        }
+
         dispatch(listProductDetails(match.params.id))
-    }, [dispatch]);
+    }, [dispatch, match, successProductReview]);
 
     // button add to Cart handler
     const addToCartHandler = () => {
@@ -51,7 +58,14 @@ const ProductScreen = ( {history, match} ) => {
 
     }
 
-    
+    const submitHandler = (e) => {
+        // console.log('Submit send comment! OK!')
+        e.preventDefault();
+        dispatch( createProductReview(match.params.id, {
+            rating,
+            comment
+        }) )
+    }
     
     return (
         <>
@@ -174,12 +188,40 @@ const ProductScreen = ( {history, match} ) => {
                                 ))}
                                 <ListGroup.Item>
                                     <h2>Write a Customer Review</h2>
+                                    {errorProductReview && <Message variant='danger'>{errorProductReview}</Message>}
+
                                     {userInfo ? (
                                         <Form onSubmit={submitHandler}>
                                             <Form.Group controlId='rating'>
                                                 <Form.Label>Rating</Form.Label>
+                                                <Form.Control 
+                                                    as='select' 
+                                                    value={rating}
+                                                    onChange={ (e) => setRating(e.target.value)}
+                                                >
+                                                    <option value=''>Select...</option>
+                                                    <option value='1'>1 - Poor</option>
+                                                    <option value='2'>2 - Fair</option>
+                                                    <option value='3'>3 - Good</option>
+                                                    <option value='4'>4 - Very Good</option>
+                                                    <option value='5'>5 - Excellent</option>
 
+                                                </Form.Control>
                                             </Form.Group>
+                                            <Form.Group controlId='comment'>
+                                                <Form.Label>Comment</Form.Label>
+                                                <Form.Control 
+                                                    as='textarea'
+                                                    row='3'
+                                                    value={comment}
+                                                    onChange={ (e) => setComment(e.target.value)}
+                                                >
+                                                </Form.Control>
+                                            </Form.Group>
+                                            <Button type='submit' variant='primary'>
+                                                Submit
+                                            </Button>
+
                                         </Form>
                                     )
                                     : <Message var='warning'>
