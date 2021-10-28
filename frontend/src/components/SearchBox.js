f2a626ad4a1fb7f8970793cbf7913eabbe60e3c3
AlexanderMukas <alexandermukas@yahoo.com>
2021-10-28 20:32:01 +0300

    small fix submit handler of search commit

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
 
-const SearchBox = () => {
+const SearchBox = ( { history } ) => {
     const [keyword, setKeyword] = useState('');
 
     const submitHandler = (e) => {
         e.preventDefault()
-        
+        if( keyword.trim() ) {
+            history.push(`/search/${keyword}`);
+        } else {
+            history.push(`/`);
+        }
     }
 
     return (
